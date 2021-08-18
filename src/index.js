bf54f51763ba94cc7fece2566f87f78c840ab9c1
roger chat <mattrogerpelosi@gmail.com>
2021-08-18 19:24:12 -0400

    Additional Deliverable and code clean

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const BASEURL = 'http://localhost:3000/ramens';
+//-------- SAME VARIABLES USED BY SEPARATE FUNCTIONS --------//
+const form = document.querySelector('#new-ramen');
+const editForm = document.querySelector('#edit-ramen');
 
-const form = document.querySelector('form');
-const divEl = document.querySelector('div#ramen-menu')
+const divEl = document.querySelector('div#ramen-menu');
 
-const detailImg = document.querySelector('div#ramen-detail img')
-const detailH2 = document.querySelector('div#ramen-detail h2')
-const detailH3 = document.querySelector('div#ramen-detail h3')
 const rating = document.querySelector('#rating');
 const comment = document.querySelector('#comment');
 
-fetch(BASEURL)
+
+//-------- INITIAL RAMEN IMAGES --------//
+fetch('http://localhost:3000/ramens')
 .then(resp => resp.json())
 .then(array => {
     for(ramens of array){
@@ -17,13 +17,15 @@ fetch(BASEURL)
     }
 })
 
-form.addEventListener('submit', (e) => {
+
+//-------- EVENT LISTENERS --------//
+form.addEventListener('submit', e => {
     e.preventDefault();
-    const newName = document.querySelector('form #new-name').value;
-    const newRest = document.querySelector('form #new-restaurant').value;
+    // const newName = document.querySelector('form #new-name').value;
+    // const newRest = document.querySelector('form #new-restaurant').value;
     const newRamImg = document.querySelector('form #new-image').value;
-    const newRate = document.querySelector('form #new-rating').value;
-    const newComm = document.querySelector('form #new-comment').value;
+    // const newRate = document.querySelector('form #new-rating').value;
+    // const newComm = document.querySelector('form #new-comment').value;
     // console.log(newName)
     // console.log(newName);
     // const newRamObject = {
@@ -37,10 +39,19 @@ form.addEventListener('submit', (e) => {
     newSubImg.src = newRamImg;
     divEl.appendChild(newSubImg);
     form.reset();
-    
+});
 
-})
+editForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const newRating = document.querySelector('#new-rating2');
+    const newComment = document.querySelector('#new-comment2').value;
+    rating.textContent = newRating.value;
+    comment.textContent = newComment;
+    editForm.reset();
+});
 
+
+//-------- ADDING IMAGES AND LISTENER IN SUIT --------//
 function createImg(obj) {
     const newImage = document.createElement('img');
     newImage.src = obj.image;
@@ -51,8 +62,11 @@ function createImg(obj) {
         fetch(`http://localhost:3000/ramens/${newImage.id}`)
         .then(resp => resp.json())
         .then(data => {
+            const detailImg = document.querySelector('div#ramen-detail img');
             detailImg.src = data.image;
+            const detailH2 = document.querySelector('div#ramen-detail h2');
             detailH2.textContent = data.name;
+            const detailH3 = document.querySelector('div#ramen-detail h3');
             detailH3.textContent = data.restaurant;
             rating.textContent = data.rating;
             comment.textContent = data.comment;
