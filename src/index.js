f4af5457342358a23fa60288dfcd232a893f0895
roger chat <mattrogerpelosi@gmail.com>
2021-08-18 12:14:25 -0400

    First three delivs

--- src/index.js
+++ src/index.js
@@ -1,58 +1,62 @@
-// write your code here
-fetch(' http://localhost:3000/ramens')
-    .then(r => r.json())
-    .then(arr => {
-        arr.forEach(ramen => {
-            renderNewRamen(ramen)
-        });
-    })
+const BASEURL = 'http://localhost:3000/ramens';
 
+const form = document.querySelector('form');
+const divEl = document.querySelector('div#ramen-menu')
 
-function renderNewRamen(ramen) {
-    const img = document.createElement('img')
-    img.src = ramen.image
-    document.querySelector('#ramen-menu').append(img)
+const detailImg = document.querySelector('div#ramen-detail img')
+const detailH2 = document.querySelector('div#ramen-detail h2')
+const detailH3 = document.querySelector('div#ramen-detail h3')
+const rating = document.querySelector('#rating');
+const comment = document.querySelector('#comment');
 
-    img.addEventListener('click', e => {
-        const ramenDetail = document.querySelector("#ramen-detail")
-        const detailImg = ramenDetail.querySelector('.detail-image')
-        detailImg.src = ramen.image
-
-        const h2 = ramenDetail.querySelector('h2.name')
-        h2.textContent = ramen.name
-
-        const h3 = ramenDetail.querySelector('h3.restaurant')
-        h3.textContent = ramen.restaurant
-
-        const rating = document.querySelector("body > p:nth-child(5) > span")
-        rating.textContent = ramen.rating
-
-        const comment = document.querySelector("body > p:nth-child(7)")
-        comment.textContent = ramen.comment
-    })
-}
-// document.querySelector('#ramen-menu').addEventListener('click', e => {
-//     if(e.target.matches('')) {
-
-//     }
-// })
-
-
-
-
-const form = document.querySelector("#new-ramen")
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    const name = e.target[0].value
-    const restaurant = e.target[1].value
-    const image = e.target[2].value
-    const rating = e.target[3].value
-    const comment = e.target[4].value
-
-    const ramen = { name, restaurant, image, rating, comment }
-    console.log(ramen)
-    renderNewRamen(ramen)
-    e.target.reset()
+fetch(BASEURL)
+.then(resp => resp.json())
+.then(array => {
+    for(ramens of array){
+        createImg(ramens);
+    }
 })
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const newName = document.querySelector('form #new-name').value;
+    const newRest = document.querySelector('form #new-restaurant').value;
+    const newRamImg = document.querySelector('form #new-image').value;
+    const newRate = document.querySelector('form #new-rating').value;
+    const newComm = document.querySelector('form #new-comment').value;
+    // console.log(newName)
+    // console.log(newName);
+    // const newRamObject = {
+    //     name: newName,
+    //     restaurant: newRest,
+    //     image: newRamImg,
+    //     rating: newRate,
+    //     comment: newComm
+    // }
+    const newSubImg = document.createElement('img');
+    newSubImg.src = newRamImg;
+    divEl.appendChild(newSubImg);
+    form.reset();
+    
 
+})
+
+function createImg(obj) {
+    const newImage = document.createElement('img');
+    newImage.src = obj.image;
+    newImage.id = obj.id;
+    divEl.appendChild(newImage);
+
+    newImage.addEventListener('click', (e) => {
+        fetch(`http://localhost:3000/ramens/${newImage.id}`)
+        .then(resp => resp.json())
+        .then(data => {
+            detailImg.src = data.image;
+            detailH2.textContent = data.name;
+            detailH3.textContent = data.restaurant;
+            rating.textContent = data.rating;
+            comment.textContent = data.comment;
+        })
+    })
+
+}
